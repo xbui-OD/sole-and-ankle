e5f38935d09a32f1caab9ff7d2d229922f18137b
Xuan Bui <xbui@overdrive.com>
2022-04-26 10:15:34 -0700

    Build header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -27,11 +27,17 @@ const Header = () => {
 };
 
 const MainHeader = styled.div`
-  padding: 0 32px;
+  padding: 20px 32px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  display: flex;
+  align-items: center;
 `;
 
-const Nav = styled.nav``;
+const Nav = styled.nav`
+  display: flex;
+  gap: 48px;
+  margin: auto;
+`;
 
 const NavLink = styled.a`
   font-size: 1.125rem;
